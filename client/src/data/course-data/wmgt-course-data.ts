418a2e7479a5f94efe4764025192cc604bf92f95
seantokuzo <seantokuzo@gmail.com>
2022-11-28 23:39:16 -0800

    add mojis

--- client/src/data/course-data/wmgt-course-data.ts
+++ client/src/data/course-data/wmgt-course-data.ts
@@ -47,10 +47,29 @@ export type CourseAlias =
   | 'MYE'
   | 'MYH'
 
+export enum CourseMoji {
+  TouristTrap = '🌴',
+  CherryBlossom = '🌸',
+  SeagullStacks = '🪶',
+  ArizonaModern = '🌵',
+  OriginalGothic = '🪦',
+  TethysStation = '🛰',
+  BogeysBonanza = '🤠',
+  QuixoteValley = '💨',
+  Sweetopia = '🍭',
+  GardensofBabylon = '🌱',
+  ShangriLa = '🏮',
+  ElDorado = '🐊',
+  Labyrinth = '🦉',
+  TwentyK = '🐙',
+  Myst = '📚'
+}
+
 export interface CourseInterface {
   course: CourseList
   difficulty: 'Easy' | 'Hard'
   alias: CourseAlias
+  courseMoji: CourseMoji
   par: number
   parByHole: number[]
 }
@@ -60,6 +79,7 @@ export const courseData: CourseInterface[] = [
     course: 'Tourist Trap',
     difficulty: 'Easy',
     alias: 'TTE',
+    courseMoji: CourseMoji.TouristTrap,
     par: 57,
     parByHole: [2, 2, 3, 3, 3, 3, 3, 4, 3, 3, 3, 3, 3, 4, 3, 3, 3, 6]
   },
@@ -67,6 +87,7 @@ export const courseData: CourseInterface[] = [
     course: 'Tourist Trap',
     difficulty: 'Hard',
     alias: 'TTH',
+    courseMoji: CourseMoji.TouristTrap,
     par: 61,
     parByHole: [2, 3, 3, 3, 3, 4, 3, 3, 4, 3, 3, 3, 4, 4, 4, 3, 3, 6]
   },
@@ -74,6 +95,7 @@ export const courseData: CourseInterface[] = [
     course: 'Cherry Blossom',
     difficulty: 'Easy',
     alias: 'CBE',
+    courseMoji: CourseMoji.CherryBlossom,
     par: 63,
     parByHole: [3, 4, 3, 4, 3, 3, 3, 4, 3, 5, 3, 4, 2, 3, 3, 3, 3, 7]
   },
@@ -81,6 +103,7 @@ export const courseData: CourseInterface[] = [
     course: 'Cherry Blossom',
     difficulty: 'Hard',
     alias: 'CBH',
+    courseMoji: CourseMoji.CherryBlossom,
     par: 67,
     parByHole: [3, 4, 3, 4, 3, 3, 3, 4, 3, 8, 3, 3, 2, 4, 3, 4, 4, 6]
   },
@@ -88,6 +111,7 @@ export const courseData: CourseInterface[] = [
     course: 'Seagull Stacks',
     difficulty: 'Easy',
     alias: 'SSE',
+    courseMoji: CourseMoji.SeagullStacks,
     par: 62,
     parByHole: [2, 3, 3, 4, 2, 5, 4, 3, 3, 4, 3, 3, 4, 3, 5, 3, 3, 5]
   },
@@ -95,6 +119,7 @@ export const courseData: CourseInterface[] = [
     course: 'Seagull Stacks',
     difficulty: 'Hard',
     alias: 'SSH',
+    courseMoji: CourseMoji.SeagullStacks,
     par: 67,
     parByHole: [2, 3, 4, 3, 4, 5, 4, 3, 4, 4, 3, 3, 5, 3, 5, 4, 3, 5]
   },
@@ -102,6 +127,7 @@ export const courseData: CourseInterface[] = [
     course: 'Arizona Modern',
     difficulty: 'Easy',
     alias: 'AME',
+    courseMoji: CourseMoji.ArizonaModern,
     par: 66,
     parByHole: [3, 3, 4, 4, 4, 3, 3, 5, 4, 4, 4, 5, 4, 3, 3, 3, 3, 4]
   },
@@ -109,6 +135,7 @@ export const courseData: CourseInterface[] = [
     course: 'Arizona Modern',
     difficulty: 'Hard',
     alias: 'AMH',
+    courseMoji: CourseMoji.ArizonaModern,
     par: 65,
     parByHole: [3, 3, 4, 4, 3, 3, 3, 5, 4, 4, 4, 5, 4, 3, 3, 3, 3, 4]
   },
@@ -116,6 +143,7 @@ export const courseData: CourseInterface[] = [
     course: 'Original Gothic',
     difficulty: 'Easy',
     alias: 'OGE',
+    courseMoji: CourseMoji.OriginalGothic,
     par: 54,
     parByHole: [2, 3, 3, 3, 2, 3, 3, 3, 2, 3, 4, 3, 3, 3, 3, 4, 4, 3]
   },
@@ -123,6 +151,7 @@ export const courseData: CourseInterface[] = [
     course: 'Original Gothic',
     difficulty: 'Hard',
     alias: 'OGH',
+    courseMoji: CourseMoji.OriginalGothic,
     par: 64,
     parByHole: [2, 3, 3, 4, 3, 3, 3, 4, 4, 4, 5, 4, 3, 3, 3, 4, 5, 4]
   },
@@ -130,6 +159,7 @@ export const courseData: CourseInterface[] = [
     course: 'Tethys Station',
     difficulty: 'Easy',
     alias: 'TSE',
+    courseMoji: CourseMoji.TethysStation,
     par: 63,
     parByHole: [2, 3, 3, 4, 3, 3, 4, 5, 3, 3, 3, 4, 3, 4, 3, 4, 5, 4]
   },
@@ -137,6 +167,7 @@ export const courseData: CourseInterface[] = [
     course: 'Tethys Station',
     difficulty: 'Hard',
     alias: 'TSH',
+    courseMoji: CourseMoji.TethysStation,
     par: 64,
     parByHole: [3, 3, 4, 3, 4, 3, 4, 5, 3, 3, 3, 3, 3, 3, 4, 4, 5, 4]
   },
@@ -144,6 +175,7 @@ export const courseData: CourseInterface[] = [
     course: "Bogey's Bonanza",
     difficulty: 'Easy',
     alias: 'BBE',
+    courseMoji: CourseMoji.BogeysBonanza,
     par: 60,
     parByHole: [2, 3, 4, 3, 4, 4, 3, 4, 4, 3, 2, 4, 4, 3, 4, 3, 3, 3]
   },
@@ -151,6 +183,7 @@ export const courseData: CourseInterface[] = [
     course: "Bogey's Bonanza",
     difficulty: 'Hard',
     alias: 'BBH',
+    courseMoji: CourseMoji.BogeysBonanza,
     par: 67,
     parByHole: [3, 3, 5, 4, 4, 3, 3, 4, 4, 3, 2, 5, 4, 3, 5, 3, 3, 6]
   },
@@ -158,6 +191,7 @@ export const courseData: CourseInterface[] = [
     course: 'Quixote Valley',
     difficulty: 'Easy',
     alias: 'QVE',
+    courseMoji: CourseMoji.QuixoteValley,
     par: 54,
     parByHole: [2, 2, 3, 4, 3, 3, 3, 3, 3, 3, 2, 3, 3, 4, 3, 3, 3, 4]
   },
@@ -165,6 +199,7 @@ export const courseData: CourseInterface[] = [
     course: 'Quixote Valley',
     difficulty: 'Hard',
     alias: 'QVH',
+    courseMoji: CourseMoji.QuixoteValley,
     par: 60,
     parByHole: [3, 3, 3, 5, 4, 3, 3, 3, 3, 3, 3, 3, 4, 4, 3, 3, 3, 4]
   },
@@ -172,6 +207,7 @@ export const courseData: CourseInterface[] = [
     course: 'Sweetopia',
     difficulty: 'Easy',
     alias: 'SWE',
+    courseMoji: CourseMoji.Sweetopia,
     par: 55,
     parByHole: [2, 3, 4, 3, 3, 2, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 2, 4]
   },
@@ -179,6 +215,7 @@ export const courseData: CourseInterface[] = [
     course: 'Sweetopia',
     difficulty: 'Hard',
     alias: 'SWH',
+    courseMoji: CourseMoji.Sweetopia,
     par: 59,
     parByHole: [3, 3, 4, 3, 3, 3, 4, 4, 3, 3, 3, 3, 3, 2, 4, 4, 3, 4]
   },
@@ -186,6 +223,7 @@ export const courseData: CourseInterface[] = [
     course: 'Gardens of Babylon',
     difficulty: 'Easy',
     alias: 'GBE',
+    courseMoji: CourseMoji.GardensofBabylon,
     par: 61,
     parByHole: [2, 3, 3, 4, 3, 5, 2, 3, 6, 3, 4, 4, 3, 4, 3, 3, 3, 3]
   },
@@ -193,6 +231,7 @@ export const courseData: CourseInterface[] = [
     course: 'Gardens of Babylon',
     difficulty: 'Hard',
     alias: 'GBH',
+    courseMoji: CourseMoji.GardensofBabylon,
     par: 64,
     parByHole: [3, 3, 4, 3, 4, 6, 3, 3, 3, 4, 4, 4, 3, 3, 3, 3, 4, 4]
   },
@@ -200,6 +239,7 @@ export const courseData: CourseInterface[] = [
     course: 'Shangri-La',
     difficulty: 'Easy',
     alias: 'SLE',
+    courseMoji: CourseMoji.ShangriLa,
     par: 64,
     parByHole: [2, 3, 3, 4, 4, 3, 4, 3, 3, 4, 3, 5, 3, 5, 4, 3, 3, 5]
   },
@@ -207,6 +247,7 @@ export const courseData: CourseInterface[] = [
     course: 'Shangri-La',
     difficulty: 'Hard',
     alias: 'SLH',
+    courseMoji: CourseMoji.ShangriLa,
     par: 68,
     parByHole: [4, 4, 2, 3, 4, 4, 4, 3, 4, 4, 3, 3, 3, 5, 5, 3, 4, 6]
   },
@@ -214,6 +255,7 @@ export const courseData: CourseInterface[] = [
     course: 'El Dorado',
     difficulty: 'Easy',
     alias: 'EDE',
+    courseMoji: CourseMoji.ElDorado,
     par: 62,
     parByHole: [3, 3, 3, 3, 4, 3, 3, 3, 5, 3, 5, 4, 3, 3, 3, 3, 3, 5]
   },
@@ -221,6 +263,7 @@ export const courseData: CourseInterface[] = [
     course: 'El Dorado',
     difficulty: 'Hard',
     alias: 'EDH',
+    courseMoji: CourseMoji.ElDorado,
     par: 61,
     parByHole: [3, 3, 3, 3, 4, 4, 3, 3, 5, 3, 4, 3, 3, 3, 3, 3, 3, 5]
   },
@@ -228,6 +271,7 @@ export const courseData: CourseInterface[] = [
     course: 'Labyrinth',
     difficulty: 'Easy',
     alias: 'LBE',
+    courseMoji: CourseMoji.Labyrinth,
     par: 64,
     parByHole: [3, 3, 4, 5, 3, 3, 3, 4, 4, 3, 3, 3, 3, 4, 4, 3, 3, 6]
   },
@@ -235,6 +279,7 @@ export const courseData: CourseInterface[] = [
     course: 'Labyrinth',
     difficulty: 'Hard',
     alias: 'LBH',
+    courseMoji: CourseMoji.Labyrinth,
     par: 65,
     parByHole: [3, 3, 4, 5, 3, 3, 3, 3, 5, 3, 3, 4, 3, 5, 4, 3, 4, 4]
   },
@@ -242,6 +287,7 @@ export const courseData: CourseInterface[] = [
     course: '20,000 Leagues Under the Sea',
     difficulty: 'Easy',
     alias: '20E',
+    courseMoji: CourseMoji.TwentyK,
     par: 59,
     parByHole: [3, 3, 3, 3, 3, 3, 4, 4, 3, 4, 3, 3, 4, 3, 4, 3, 3, 3]
   },
@@ -249,6 +295,7 @@ export const courseData: CourseInterface[] = [
     course: '20,000 Leagues Under the Sea',
     difficulty: 'Hard',
     alias: '20H',
+    courseMoji: CourseMoji.TwentyK,
     par: 60,
     parByHole: [3, 3, 3, 3, 3, 3, 4, 4, 3, 4, 3, 3, 4, 3, 4, 3, 3, 4]
   },
@@ -256,6 +303,7 @@ export const courseData: CourseInterface[] = [
     course: 'Myst',
     difficulty: 'Easy',
     alias: 'MYE',
+    courseMoji: CourseMoji.Myst,
     par: 61,
     parByHole: [3, 3, 3, 3, 4, 3, 4, 3, 4, 3, 3, 3, 4, 3, 4, 3, 4, 4]
   },
@@ -263,6 +311,7 @@ export const courseData: CourseInterface[] = [
     course: 'Myst',
     difficulty: 'Hard',
     alias: 'MYH',
+    courseMoji: CourseMoji.Myst,
     par: 63,
     parByHole: [3, 3, 3, 3, 3, 3, 5, 3, 4, 3, 3, 4, 4, 3, 4, 3, 4, 5]
   }
