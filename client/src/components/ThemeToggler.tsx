46ba74455750b01f48eee542d1f6fd747dc0b60b
seantokuzo <seantokuzo@gmail.com>
2022-11-29 01:19:11 -0800

    darkmode toggler scorecard colors

--- client/src/components/ThemeToggler.tsx
+++ client/src/components/ThemeToggler.tsx
@@ -16,9 +16,20 @@ const ThemeToggler: React.FC = () => {
         id="default-toggle"
         className="sr-only peer"
       />
-      <div className="w-11 h-6 bg-zinc-500  peer-focus:outline-none rounded-full peer dark:bg-zinc-500  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:border-2 peer-checked:border-zinc-600 peer-checked:bg-zinc-600"></div>
+      <div
+        className="w-11 h-6 rounded-full
+          peer-checked:after:bg-[#38280e] peer-checked:after:border-[#38280e]
+          after:bg-[#f8ff71] after:border-[#f8ff71]
+          bg-[#38280e] peer-checked:bg-[#f8ff71]
+          peer-checked:border-[#f8ff71]
+          after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:rounded-full after:h-5 after:w-5 after:transition-all
+          peer-checked:border-2 peer-focus:outline-none peer peer-checked:after:translate-x-full"
+      ></div>
     </label>
   )
 }
 
 export default ThemeToggler
+
+// #f8ff71
+// #38280e
