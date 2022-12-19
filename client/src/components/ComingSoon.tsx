4e133cfcbd68e993b817695f25102fa469a6b7c3
seantokuzo <seantokuzo@gmail.com>
2022-12-18 19:41:51 -0800

    ok ill stop now

--- client/src/components/ComingSoon.tsx
+++ client/src/components/ComingSoon.tsx
@@ -6,7 +6,7 @@ const ComingSoon: React.FC<Props> = ({ text }) => {
   return (
     <div
       className="w-full max-w-xl p-8 my-8 font-bold text-center
-      text-xl md:text-2xl lg:text-4xl 
+      text-xl md:text-2xl lg:text-4xl uppercase
       rounded-md animate-bounce
       border-4 border-[#f8ff71]
       text-[#38280e] bg-[#f8ff71] shadow-inyellfocus
