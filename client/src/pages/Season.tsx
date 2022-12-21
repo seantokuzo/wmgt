6848b96c0cf1441ba8745cd5da7af86e50a1e63d
seantokuzo <seantokuzo@gmail.com>
2022-12-20 17:00:15 -0800

    zero out scorecard view state on round change

--- client/src/pages/Season.tsx
+++ client/src/pages/Season.tsx
@@ -2,9 +2,19 @@ import { Outlet, useLocation } from 'react-router-dom'
 import SeasonMenu from '../components/season/SeasonMenu'
 import { season7Data } from 'data/round-data/s7-round-data'
 import { season6Data } from 'data/round-data/s6-round-data'
+import { useEffect } from 'react'
+import { useSeasonContext } from 'context/season/seasonContext'
 
 const Season: React.FC = () => {
   const { pathname } = useLocation()
+  const { viewCourse, viewFrontNine, viewScorecard } = useSeasonContext()
+
+  useEffect(() => {
+    viewCourse('easy')
+    viewFrontNine()
+    viewScorecard()
+    // eslint-disable-next-line
+  }, [pathname])
 
   return (
     <div className="w-full flex flex-col justify-center items-center">
