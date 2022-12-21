54ceb378a01e826c6acdc1030b6ba729b92d5dcc
seantokuzo <seantokuzo@gmail.com>
2022-12-21 02:57:42 -0800

    add bg img to course details

--- client/src/pages/Course.tsx
+++ client/src/pages/Course.tsx
@@ -1,11 +1,27 @@
 import CoursesMenu from '../components/course/CoursesMenu'
 import { Outlet, useLocation } from 'react-router-dom'
+import { courseFullImgLink } from 'data/course-data/wmgt-course-data'
 
 const Course: React.FC = () => {
   const { pathname } = useLocation()
 
+  const bgImg =
+    pathname !== '/course'
+      ? `url(${courseFullImgLink.replaceAll(
+          '<COURSE>',
+          pathname.split('/course/')[1].toUpperCase()
+        )})`
+      : ''
+  console.log(bgImg)
+
   return (
-    <div className="w-full flex flex-col justify-center items-center">
+    <div
+      className="w-full bg-no-repeat bg-cover bg-center h-auto md:h-[90vh]
+      flex flex-col justify-center items-center"
+      style={{
+        backgroundImage: bgImg
+      }}
+    >
       {pathname === '/course' && <CoursesMenu />}
       <Outlet />
     </div>
