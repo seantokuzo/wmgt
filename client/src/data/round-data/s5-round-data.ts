47dd7983782383ecec32f3f480ed4289ffa6061d
seantokuzo <seantokuzo@gmail.com>
2023-02-01 14:17:47 -0800

    start s5 round data - add courses

---
+++ client/src/data/round-data/s5-round-data.ts
@@ -0,0 +1,16 @@
+import { RoundDataInterface } from './roundTypes'
+
+export const season6Data: RoundDataInterface[] = [
+  // { season: 5, round: 1, easyCourse: 'OGE', hardCourse: 'AMH', scores: [] },
+  // { season: 5, round: 2, easyCourse: 'GBE', hardCourse: 'CBH', scores: [] },
+  // { season: 5, round: 3, easyCourse: 'SSE', hardCourse: 'TSH', scores: [] },
+  // { season: 5, round: 4, easyCourse: 'BBE', hardCourse: 'OGH', scores: [] },
+  // { season: 5, round: 5, easyCourse: 'CBE', hardCourse: 'QVH', scores: [] },
+  // { season: 5, round: 6, easyCourse: 'SLE', hardCourse: 'BBH', scores: [] },
+  // { season: 5, round: 7, easyCourse: 'SWE', hardCourse: 'GBH', scores: [] },
+  // { season: 5, round: 8, easyCourse: 'AME', hardCourse: 'SSH', scores: [] },
+  // { season: 5, round: 9, easyCourse: 'TSE', hardCourse: 'SWH', scores: [] },
+  // { season: 5, round: 10, easyCourse: 'TTE', hardCourse: 'TTH', scores: [] },
+  // { season: 5, round: 11, easyCourse: 'QVE', hardCourse: 'SLH', scores: [] },
+  // { season: 5, round: 12, easyCourse: 'EDE', hardCourse: 'EDH', scores: [] },
+]
