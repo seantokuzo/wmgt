9beba96fc20b1e4d8d1c9eea84b486bfa2708797
seantokuzo <seantokuzo@gmail.com>
2023-02-03 13:52:45 -0800

    no coconuts component

---
+++ client/src/components/scorecard/NoCoconuts.tsx
@@ -0,0 +1,15 @@
+const NoCoconuts = () => {
+  return (
+    <div className="flex flex-col justify-center items-center mt-3">
+      <div className="mt-2 px-3 py-2 bg-sh-silver brdr-silver border-2 rounded-md text-black font-bold font-scorenum">
+        😢 No Coconuts 😭
+      </div>
+      <img
+        src="https://c.tenor.com/2n7n23BkVM0AAAAC/happy-gilmore-get-me-outta-here.gif"
+        className="w-[80%] max-w-xl my-4 rounded-lg"
+      ></img>
+    </div>
+  )
+}
+
+export default NoCoconuts
