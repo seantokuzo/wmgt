fcc0a902d25e06e6dc705f3966725e25ea2c34de
seantokuzo <seantokuzo@gmail.com>
2023-02-05 20:16:02 -0800

    scroll to top for where art thou

--- client/src/pages/WhereArtThou.tsx
+++ client/src/pages/WhereArtThou.tsx
@@ -1,7 +1,12 @@
+import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const WhereArtThou: React.FC = () => {
   const navigate = useNavigate()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
 
   return (
     <section
