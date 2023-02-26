576856abc7874959a192c76b13d617cd4398a815
seantokuzo <seantokuzo@gmail.com>
2023-02-26 02:47:32 -0800

    comment out unused imports

--- client/src/data/playerDataGod.ts
+++ client/src/data/playerDataGod.ts
@@ -1,29 +1,29 @@
 import { DataGod } from './dataGod'
-import { CourseAlias, courseData } from './course-data/wmgt-course-data'
-import { allPlayersList, PlayerInterface } from './player-data/AllPlayersList'
+// import { CourseAlias, courseData } from './course-data/wmgt-course-data'
+// import { allPlayersList, PlayerInterface } from './player-data/AllPlayersList'
 import {
-  BadgeRound,
-  PlayerRoundInterface,
+  // BadgeRound,
+  // PlayerRoundInterface,
   RoundDataInterface,
   RoundIdentifier
 } from './round-data/roundTypes'
-import { season1OfficialResults } from './season-data/season1OfficialResults'
-import { season2OfficialResults } from './season-data/season2OfficialResults'
-import { season3OfficialResults } from './season-data/season3OfficialResults'
-import { season4OfficialResults } from './season-data/season4OfficialResults'
-import { season5OfficialResults } from './season-data/season5OfficialResults'
-import { season6OfficialResults } from './season-data/season6OfficialResults'
-import { season7OfficialResults } from './season-data/season7OfficialResults'
-import { season8OfficialResults } from './season-data/season8OfficialResults'
+// import { season1OfficialResults } from './season-data/season1OfficialResults'
+// import { season2OfficialResults } from './season-data/season2OfficialResults'
+// import { season3OfficialResults } from './season-data/season3OfficialResults'
+// import { season4OfficialResults } from './season-data/season4OfficialResults'
+// import { season5OfficialResults } from './season-data/season5OfficialResults'
+// import { season6OfficialResults } from './season-data/season6OfficialResults'
+// import { season7OfficialResults } from './season-data/season7OfficialResults'
+// import { season8OfficialResults } from './season-data/season8OfficialResults'
 import { season5Data } from './round-data/s5-round-data'
 import { season6Data } from './round-data/s6-round-data'
 import { season7Data } from './round-data/s7-round-data'
 import { season8Data } from './round-data/s8-round-data'
-import {
-  PlayerSeasonResultsOfficial,
-  SeasonResultsOfficial
-} from './season-data/seasonResultsTypes'
-import { CURRENT_SEASON } from 'utils/constants'
+// import {
+//   PlayerSeasonResultsOfficial,
+//   SeasonResultsOfficial
+// } from './season-data/seasonResultsTypes'
+// import { CURRENT_SEASON } from 'utils/constants'
 
 export abstract class PlayerDataGod {
   static getAllData = () => {
