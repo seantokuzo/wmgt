20ebf1509d657b18caf5529a303d4d0bffe00eb3
seantokuzo <seantokuzo@gmail.com>
2023-04-11 14:49:44 -0700

    prepare for season 9 data

---
+++ client/src/data/round-data/s9-round-data.ts
@@ -0,0 +1,16 @@
+import { RoundDataInterface } from './roundTypes'
+
+export const season9Data: RoundDataInterface[] = [
+  // { season: 9, round: 1, easyCourse: '', hardCourse: '', scores: [] }
+  // { season: 9, round: 2, easyCourse: '', hardCourse: '', scores: [] },
+  // { season: 9, round: 3, easyCourse: '', hardCourse: '', scores: [] },
+  // { season: 9, round: 4, easyCourse: '', hardCourse: '', scores: [] },
+  // { season: 9, round: 5, easyCourse: '', hardCourse: '', scores: [] },
+  // { season: 9, round: 6, easyCourse: '', hardCourse: '', scores: [] },
+  // { season: 9, round: 7, easyCourse: '', hardCourse: '', scores: [] },
+  // { season: 9, round: 8, easyCourse: '', hardCourse: '', scores: [] },
+  // { season: 9, round: 9, easyCourse: '', hardCourse: '', scores: [] },
+  // { season: 9, round: 10, easyCourse: '', hardCourse: '', scores: [] },
+  // { season: 9, round: 11, easyCourse: '', hardCourse: '', scores: [] },
+  // { season: 9, round: 12, easyCourse: '', hardCourse: '', scores: [] },
+]
---
+++ client/src/data/season-data/season9OfficialResults.ts
@@ -0,0 +1,6 @@
+import { SeasonResultsOfficial } from './seasonResultsTypes'
+
+export const season9OfficialResults: SeasonResultsOfficial = {
+  season: 9,
+  results: []
+}
